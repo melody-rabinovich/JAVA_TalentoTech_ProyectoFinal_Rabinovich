e3b852d759ce20e54a90ee29b81f4266b871bb24
melody-rabinovich <melodyrabinovich@gmail.com>
2025-12-05 09:09:50 -0300

    Model completo

--- src/main/java/models/Articulo.java
+++ src/main/java/models/Articulo.java
@@ -1,20 +1,35 @@
 package models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "articulos") // Mapea a la tabla "articulo"
+@Entity 
+@Table(name = "articulos") 
+
 public class Articulo {
 
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // por cada atributo, declaro la columna
+    @Column (name = "id") 
+    private Long id;   //  la PK autoincremental
+    @Column(name = "nombre", nullable = false, length = 200)
     private String nombre;
+    @Column(name="precio", nullable = false)
     private double precio;
 
     Articulo(long id, String nombre, double precio){
         setId(id); 
         setNombre(nombre);
         setPrecio(precio);
+    }
+
+    Articulo (){
+        // Constructor sobrecargado vacío, requerido por JPA (?)
     }
 
     
